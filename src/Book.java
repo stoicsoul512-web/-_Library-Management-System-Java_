8f0a42d8ab78ce4712155ec154db8673c6e117d1
stoicsoul512-web <stoicsoul512@gmail.com>
2025-11-05 18:48:30 +0530

    Added Book.java - defines Book class with id, title, author

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    int id;
+    String title;
+    String author;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+}
